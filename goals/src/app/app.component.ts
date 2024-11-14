66ae1586ba6ffcfb1fb3b5b1743babe09bec9154
PrinceTheDev <sinbadprince9@gmail.com>
2024-11-14 17:06:11 +0100

    Add goals as a regular folder

---
+++ goals/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { RolesComponent } from './component/roles/roles.component';
+import { MasterComponent } from './component/master/master.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, MasterComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'goals';
+}
