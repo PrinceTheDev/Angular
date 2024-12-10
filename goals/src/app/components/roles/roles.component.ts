1b8269ddbf343987f26dbd4fa181b0bcf1cbf14b
PrinceTheDev <sinbadprince9@gmail.com>
2024-12-10 15:14:28 +0100

    Worked on Structural and Attribute Directives

--- goals/src/app/components/roles/roles.component.ts
+++ goals/src/app/components/roles/roles.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { IRole } from '../master/interface/role';
 
 @Component({
   selector: 'app-roles',
@@ -8,30 +10,62 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './roles.component.html',
   styleUrl: './roles.component.css'
 })
-export class RolesComponent {
+export class RolesComponent implements OnInit{
+
+  roleList: IRole [] = []; 
+  http = inject(HttpClient);
+
+ngOnInit(): void {
+  this.getAllRoles()
+}
+
+getAllRoles() {
+  this.http.get("https://freeapi.miniprojectideas.com/api/ClientStrive/GetAllRoles").subscribe((res:any)=>{
+    this.roleList = res.data;
+  })
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   //string, number, date, boolean, array, object, null, undefined
 
-  // firstname: string = "Angular Tutorial";
-  // angularVersion = "Version 18";
+  //  firstname: string = "Angular Tutorial";
+  //  angularVersion = "Version 18";
 
-  // version: number = 18;
+  //  version: number = 18;
 
-  // isActive: boolean = false;
+  //  isActive: boolean = false;
 
-  // currentDate : Date = new Date();
+  //  currentDate : Date = new Date();
 
-  // inputType: string = "button";
-  // selectedState: string = "";
+  //  inputType: string = "button";
+  //  selectedState: string = "";
 
 
-  // showWelcomeAlert () {
-  //   alert("Welcome to Goals App")
-  // }
-  // showMessage(message: string) {
-  //   alert(message)
-  // }
+  //  showWelcomeAlert () {
+  //    alert("Welcome to Goals App")
+  //  }
+  //  showMessage(message: string) {
+  //    alert(message)
+  //  }
 
-  
+
 
 
 }
