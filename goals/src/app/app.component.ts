72540de5346a22a075c5f4e382fb88b3a3810841
PrinceTheDev <sinbadprince9@gmail.com>
2024-12-10 10:16:44 +0100

    starting over

--- goals/src/app/app.component.ts
+++ goals/src/app/app.component.ts
@@ -6,7 +6,7 @@ import { MasterComponent } from './component/master/master.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MasterComponent],
+  imports: [RouterOutlet, MasterComponent, RolesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
